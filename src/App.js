f6e13ceed7f19c221fc069c7619fc63e5799eff9
reinaldo.oliveira <reinaldo.oliveira@SUNOAPPLE144.local>
2024-09-14 09:54:18 -0300

    rnco 14/09/2024

--- src/App.js
+++ src/App.js
@@ -1,19 +1,94 @@
 import logo from './logo.svg'
 import './App.css'
-import TagManager from 'react-gtm-module'
+//import ReactGA from 'react-ga' // 1 - Library GAU direct without dataLayer
+import TagManager from 'react-gtm-module' // 2 - Library GTM to send event by dataLayer
+//import ReactGA4 from "react-ga4" // 3 - Library GA4 direct without dataLayer - https://www.npmjs.com/package/react-ga4
+
+
+//eactGA.initialize('UA-98256327-1')// 1 - Library GAU direct without dataLayer
+//ReactGA4.initialize("G-K6MBY9B33E"); // 3 - Library GA4 direct without dataLayer
+
+// GA4 - PROPERTY ID: 332041732 - MEASUREMENT ID G-DNMRELNT2D
+
+const tagManagerArgs = { gtmId: 'GTM-WV45BL3'}// 2 - Library GTM to send event by dataLayer
+TagManager.initialize(tagManagerArgs)// 2 - Library GTM to send event by dataLayer
+
+
 
 const tagManagerArgs = { gtmId: 'GTM-WV45BL3'}
 TagManager.initialize(tagManagerArgs)
 
 function App() {
+  //const TrackEvent = (action, category, label) => {// 1 - Library GAU direct without dataLayer
+    //ReactGA.event({
+      //action: action,
+      //category: category,
+      //label: label
+    //});
+    //ReactGA4.event({
+      //action: action,
+      //category: category,
+      //label: label
+   // })
+  //}
+  // const CreateDL = () => {// 2 - Library GTM to send event by dataLayer
+  //   window.dataLayer.push({
+  //     event: 'gauCustomEvent',
+  //     action: 'clickActionTest2',
+  //     category: 'menuCategoryTest2',
+  //     label: 'label2'
+  //   });
+  // }
+  // const TrackEventGa4 = (category ,action ,label ,value , nonInteraction) => { // 3 - Library GA4 direct without dataLayer
+  //   ReactGA.send("ga4CustomEvent");
+  //   ReactGA.event({
+  //     category: category,
+  //     action: action,
+  //     label: label, // optional
+  //     value: value, // optional, must be a number
+  //     nonInteraction: nonInteraction, // optional, true/false
+  //   });
+  // }
 
-  const eventClick = () => {
-    window.dataLayer.push({
-      event: 'click',
-      category: 'link-href',
-      action: 'click-cta',
-      label: 'click here',
+  //<!-- Google tag (gtag.js) -->
+  //<script async src="https://www.googletagmanager.com/gtag/js?id=G-K6MBY9B33E"></script>
+
+  //<script>
+  //    window.dataLayer = window.dataLayer || [];
+  //    function gtag(){dataLayer.push(arguments);}
+  //    gtag('js', new Date());
+
+  //    gtag('config', 'G-K6MBY9B33E');
+  //</script>
+
+  //<script>
+  //  window.gtag("event", "Test_Gtag_GA4_A", {
+  //      event_action:   "Auto Equity A",
+  //      event_category: "Email já cadastrado A",
+  //      event_label:    "ga4CustomEventGTAGLabel",
+  //      event_value:    "ga4CustomEventGTAGValue"
+  //  });
+//console.log('test 1');
+  //  gtag('event', 'Test_Gtag_GA4_B', {
+  //        event_action: 'Auto Equity B',
+  //        event_category: 'Email já cadastrado B'
+  //  });
+//console.log('test 2');
+  //</script>
+
+  const GtagEventGA4 = () => { // Create DL
+    window.gtag('event', 'cta', {
+      action:'click sem aspas',
+      category:'link href',
+      label:'click here',
+      value:'10'
     });
+    //console.log('Gtag GA4 event + propeties sent')
+    //window.dataLayer.push({
+    //  event: 'button_click',
+    //  event_category: 'Navigation',
+    //  event_label: 'Click on "My Button"',
+    //});
   }
 
   return (
@@ -28,7 +103,10 @@ function App() {
         <p>GTM-WV45BL3</p>
         <p className="App-link"
           onClick={ () => {
-            eventClick();
+            //TrackEvent('click', 'link href', 'click here'); // 1 - Library GAU direct without dataLayer
+            //CreateDL(); // 2 - Library GTM to send event by dataLayer
+            // TrackEventGa4('clickActionTestGA4', 'menuCategoryTestGA4', 'testLabelGA4', 99, true); // 3 - Library GA4 direct without dataLayer
+            GtagEventGA4() // To create a Data Layer
           }}
         >Click here!</p>
       </header>
